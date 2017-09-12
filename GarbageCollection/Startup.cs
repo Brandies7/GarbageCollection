288a0731a84f26acdb7ce781c85203836cc9a64a
Brandies7 <adanb82@gmail.com>
2017-09-12 14:38:09 -0500

    modified startup

--- GarbageCollection/Startup.cs
+++ GarbageCollection/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using GarbageCollection.Models;
 
 [assembly: OwinStartupAttribute(typeof(GarbageCollection.Startup))]
 namespace GarbageCollection
@@ -9,6 +12,29 @@ namespace GarbageCollection
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+
+        public void CreateRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+            }
         }
     }
 }
+     
